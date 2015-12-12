1111e8bd08a5397959213c8c9298734bea2950a9
Kevin Overall <kmoverall@gmail.com>
2015-12-12 13:54:12 -0500

    Initial Commit

---
+++ Assets/RobotController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotController : MonoBehaviour {
+
+    enum RobotState { Moving, Blocking, Attacking, Breaking, Dashing };
+
+    public KeyCode leftKey, rightKey;
+    public float walkSpeed, punchSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void SetState()
+    {
+
+    }
+}
